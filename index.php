05d6f2a0b05e9377eafc930f689b04dc68daaf2e
michael <michael>
2012-09-29 19:56:56 +0000

    - Update website to reflect 8.0.0rc1

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if ($fname)
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta http-equiv="Expires" content="0" />
     <meta http-equiv="Pragma" content="no-cache" />
-    <meta name="Author" content="$Id: index.php,v 1.36 2012/08/26 08:52:08 michael Exp $" />
+    <meta name="Author" content="$Id: index.php,v 1.37 2012/09/29 19:56:56 michael Exp $" />
     <meta name="Copyright" content="Copyright 2005-2012, IRCD-Hybrid Team" />
     <meta name="Description" content="IRCD-Hybrid, a high performance irc daemon" />
     <meta name="Googlebot" content="index, follow, archive" />
@@ -73,7 +73,7 @@ if ($fname)
         <p xml:lang="en">We hope that you enjoy IRCD-Hybrid and continue to use it for many years to come.</p>
         <p xml:lang="en"><br />
 	    &raquo; <a href="downloads.html">Latest STABLE release of ircd-hybrid-7:&nbsp; 7.3.1</a><br />
-            &raquo; <a href="downloads.html">Latest BETA release of ircd-hybrid-8:&nbsp; 8.0.0beta3</a><br />
+            &raquo; <a href="downloads.html">Latest BETA release of ircd-hybrid-8:&nbsp; 8.0.0rc1</a><br />
 	    &raquo; <a href="<?php echo $fname; ?>">Latest commit to CURRENT &mdash; <?php if ($delta >= 0) echo "$delta ago"; else echo "unknown"; ?></a><br />
 	</p>
     </div>
@@ -85,7 +85,7 @@ if ($fname)
         </div>
        
         <p xml:lang="en">Questions and/or Comments: <a href="mailto:&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;" xml:lang="en">&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;</a></p>
-        <p xml:lang="en">$Id: index.php,v 1.36 2012/08/26 08:52:08 michael Exp $</p>
+        <p xml:lang="en">$Id: index.php,v 1.37 2012/09/29 19:56:56 michael Exp $</p>
    </div>
 </div>
 
