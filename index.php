65c2605b4eda107d8885847ba411516c75f14a79
michael <michael>
2013-04-12 14:30:16 +0000

    - Update website

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if ($fname)
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta http-equiv="Expires" content="0" />
     <meta http-equiv="Pragma" content="no-cache" />
-    <meta name="Author" content="$Id: index.php,v 1.47 2013/01/28 19:43:03 michael Exp $" />
+    <meta name="Author" content="$Id: index.php,v 1.48 2013/04/12 14:30:16 michael Exp $" />
     <meta name="Copyright" content="Copyright 2005-2013, IRCD-Hybrid Team" />
     <meta name="Description" content="IRCD-Hybrid, a high performance irc daemon" />
     <meta name="Googlebot" content="index, follow, archive" />
@@ -72,7 +72,7 @@ if ($fname)
         <p xml:lang="en">Welcome to the official site of the IRCD-Hybrid Project! Here you'll be able to find everything you'll need to get started in hosting your own IRC server as well as support.</p>
         <p xml:lang="en">We hope that you enjoy IRCD-Hybrid and continue to use it for many years to come.</p>
         <p xml:lang="en"><br />
-	    &raquo; <a href="downloads.html">Latest STABLE release of ircd-hybrid-8:&nbsp; 8.0.6</a><br />
+	    &raquo; <a href="downloads.html">Latest STABLE release of ircd-hybrid-8:&nbsp; 8.0.7</a><br />
 <!--            &raquo; <a href="downloads.html">Latest BETA release of ircd-hybrid-8:&nbsp; 8.0.0rc1</a><br /> -->
 	    &raquo; <a href="<?php echo $fname; ?>">Latest commit to TRUNK &mdash; <?php if ($delta >= 0) echo "$delta ago"; else echo "unknown"; ?></a><br />
 	</p>
@@ -85,7 +85,7 @@ if ($fname)
         </div>
        
         <p xml:lang="en">Questions and/or Comments: <a href="mailto:&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;" xml:lang="en">&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;</a></p>
-        <p xml:lang="en">$Id: index.php,v 1.47 2013/01/28 19:43:03 michael Exp $</p>
+        <p xml:lang="en">$Id: index.php,v 1.48 2013/04/12 14:30:16 michael Exp $</p>
    </div>
 </div>
 
