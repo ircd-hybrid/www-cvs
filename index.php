a79c4967ea8d4b842b6652afd6e4e47f3dd9d010
michael <michael>
2014-07-06 14:07:09 +0000

    - Update website to reflect 8.2.0beta4 release of ircd-hybrid

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if ($fname)
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta http-equiv="Expires" content="0" />
     <meta http-equiv="Pragma" content="no-cache" />
-    <meta name="Author" content="$Id: index.php,v 1.87 2014/07/05 20:02:16 michael Exp $" />
+    <meta name="Author" content="$Id: index.php,v 1.88 2014/07/06 14:07:09 michael Exp $" />
     <meta name="Copyright" content="Copyright 2005-2014, IRCD-Hybrid Team" />
     <meta name="Description" content="IRCD-Hybrid, a high performance irc daemon" />
     <meta name="Googlebot" content="index, follow, archive" />
@@ -73,7 +73,7 @@ if ($fname)
         <p xml:lang="en">In case you're also looking for an IRC services package, we strongly recommend the Anope IRC Services.</p>
         <p xml:lang="en"><br />
 	    &raquo; <a href="downloads.html">Latest STABLE release of ircd-hybrid:&nbsp; 8.1.19</a> <br />
-            &raquo; <a href="downloads.html">Latest BETA release of ircd-hybrid:&nbsp; 8.2.0beta3</a> <br />
+            &raquo; <a href="downloads.html">Latest BETA release of ircd-hybrid:&nbsp; 8.2.0beta4</a> <br />
 	    &raquo; <a href="<?php echo $fname; ?>">Latest commit to TRUNK &mdash; <?php if ($delta >= 0) echo "$delta ago"; else echo "unknown"; ?></a><br />
 	</p>
     </div>
@@ -85,7 +85,7 @@ if ($fname)
         </div>
        
         <p xml:lang="en">Questions and/or Comments: <a href="mailto:&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;" xml:lang="en">&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;</a></p>
-        <p xml:lang="en">$Id: index.php,v 1.87 2014/07/05 20:02:16 michael Exp $</p>
+        <p xml:lang="en">$Id: index.php,v 1.88 2014/07/06 14:07:09 michael Exp $</p>
    </div>
 </div>
 
