b3771a32852a8be7690e0b741674179976261fab
michael <michael>
2014-08-09 21:44:06 +0000

    - Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if ($fname)
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta http-equiv="Expires" content="0" />
     <meta http-equiv="Pragma" content="no-cache" />
-    <meta name="Author" content="$Id: index.php,v 1.94 2014/08/09 21:17:48 michael Exp $" />
+    <meta name="Author" content="$Id: index.php,v 1.95 2014/08/09 21:44:06 michael Exp $" />
     <meta name="Copyright" content="Copyright 2005-2014, IRCD-Hybrid Team" />
     <meta name="Description" content="IRCD-Hybrid, a high performance irc daemon" />
     <meta name="Googlebot" content="index, follow, archive" />
@@ -68,6 +68,7 @@ if ($fname)
     </div>
    
     <div class="content" xml:lang="en">
+        <h2 xml:lang="en">Home ::</h2>
         <p xml:lang="en">Welcome to the official site of the IRCD-Hybrid Project! Here you'll be able to find everything you'll need to get started in hosting your own IRC server as well as support.</p>
         <p xml:lang="en">We hope that you enjoy IRCD-Hybrid and continue to use it for many years to come.</p>
         <p xml:lang="en">In case you're also looking for an IRC services package, we strongly recommend the <a href="http://www.anope.org/">Anope IRC Services</a>.</p>
@@ -93,7 +94,7 @@ if ($fname)
         </div>
        
         <p xml:lang="en">Questions and/or Comments: <a href="mailto:&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;" xml:lang="en">&#098;&#117;&#103;&#115;&#064;&#105;&#114;&#099;&#100;&#045;&#104;&#121;&#098;&#114;&#105;&#100;&#046;&#111;&#114;&#103;</a></p>
-        <p xml:lang="en">$Id: index.php,v 1.94 2014/08/09 21:17:48 michael Exp $</p>
+        <p xml:lang="en">$Id: index.php,v 1.95 2014/08/09 21:44:06 michael Exp $</p>
    </div>
 </div>
 
